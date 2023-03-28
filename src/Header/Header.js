2e3820b949736025d3a1b0f58b8556de110edc29
Nikhil Tipparthi <nreddy972@gmail.com>
2023-03-28 15:45:42 -0500

    Header changes

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="header bg-primary text-white text-center">
+      My first react app
+    </div>
+  )
+}
+
+export default Header
