2a5573bd2fd7755d4e1d7006e537d8e6194700a9
Nikhil Tipparthi <nreddy972@gmail.com>
2023-03-31 15:32:02 -0500

    Menu and Footer changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Header from './Header'
 import Footer from './Footer';
 import Menu from './Menu';
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Footer/>
       <Menu/>
+      <Footer/>
+      
+      <Home/>
+      <About/>
+      <Contact/>
     </div>
   );
 }
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,15 +1,54 @@
 import React from 'react'
 import './Menu.css'
 const Menu = () => {
-  return (
-    <div className="menu bg-secondary text-white text-end ">
-    <b>Home</b> 
-    <b>About</b> 
-    <b>Contact</b> 
-    <b>Service</b> 
+  const [myLeft, setMyLeft] = React.useState(-170)
+  const [isMobileView, setIsMobileView] = React.useState(document.body.offsetWidth < 600 ? true : false)
+  const [menu, setMenu] = React.useState()
+  let flag = true;
+  let timeoutId;
+  window.addEventListener('resize', (eve) => {
+      if (flag) {
+          fnHandleResize()
+          flag = false
+      }
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(() => {
+          fnHandleResize()
+      }, 1000)
+  })
 
-    </div>
-  )
+  const fnHandleResize = () => {
+      const _width = document.body.offsetWidth
+      setIsMobileView(_width > 600 ? false : true)
+
+  }
+
+  const fnClick = (eve) => {
+      eve.stopPropagation()
+      const {nodeName,id}=eve.target
+     if( nodeName =='DIV') return
+      
+      setMenu(id)
+      if (isMobileView) {
+          setMyLeft(-170)
+      }
+  }
+  const fnMobileMenuBtn = () => {
+      setMyLeft(myLeft == 0 ? -170 : 0)
+  }
+
+  return <div>
+      {isMobileView && <button onClick={fnMobileMenuBtn} className='mobile-menu-btn'>Mobile Menu</button>}
+      <div  onClick={fnClick} style={{ left: myLeft }} className={isMobileView ? 'mobile-menu' : 'menu'}>
+          <a id='Home' className={menu =='Home' && 'menu-active' } >HOME</a>
+          <a id='About' className={menu =='About' && 'menu-active '} >ABOUT</a>
+          <a id='Contact' className={menu =='Contact' && 'menu-active '} >CONTACT</a>
+      </div>
+
+    
+  </div>
+  
 }
+ 
 
 export default Menu
